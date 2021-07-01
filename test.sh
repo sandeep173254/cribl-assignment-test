9fd7ef900707a225851a0d06d86ced682f8e460e
skhatri <skhatri@global.ntt>
2021-06-30 19:57:09 -0500

    Initial commit for Cribl Assignment

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Deploying hosts..."
+
+echo "Deploying targets"
+
+
+docker run --hostname target_1 --net cribl_assignment --name target_1 sandeep173254/assignment:test_nodeapp_target
+docker run --hostname target_2 --net cribl_assignment --name target_2 sandeep173254/assignment:test_nodeapp_target
+
+
+echo "Removing the containers..."
+docker stop target_1
+docker stop target_2
+docker rm target_1
+docker rm target_2
