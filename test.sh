0e0d704661c5fd64f744f2e2a93e3ac369091494
skhatri <skhatri@global.ntt>
2021-06-30 20:41:19 -0500

    Update Deployment Script

--- test.sh
+++ test.sh
@@ -19,14 +19,15 @@ echo "Deploying Agent"
 
 docker run --hostname agent --net cribl_assignment --name agent sandeep173254/assignment:test_nodeapp_agent
 
-echo "Count Target_1 event.log"
-docker exec -it target_1 sh -c cat events.log | wc -l
+echo "Count Target_1 events.log"
+docker exec -it target_1 sh -c "cat events.log | wc -l"
 
-echo "Count Target_2 event.log"
-docker exec -it target_2 sh -c cat events.log | wc -l
+echo "Count Target_2 events.log"
+docker exec -it target_2 sh -c "cat events.log | wc -l"
 
 echo "Removing the containers..."
 docker stop target_1
 docker stop target_2
 docker rm target_1
 docker rm target_2
+docker rm splitter
